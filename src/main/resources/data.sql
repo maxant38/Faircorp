57f1c9f2214c2791186fd037a1ec02765776cfd4
Mariano1Schloss <mathieu.delbos34@gmail.com>
2021-11-16 18:40:58 +0100

    heatrer reconfig

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-    INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature) VALUES(-10, 'Room1', 1, 22.3, 20.0);
-    INSERT INTO ROOM(id, name, floor) VALUES(-9, 'Room2', 1);
+
+     INSERT INTO BUILDING(id,name) VALUES(3,'Administration');
+     INSERT INTO BUILDING(id,name) VALUES(2,'Gymnase');
+     INSERT INTO BUILDING(id,name) VALUES(4,'Secrétariat');
+
+
+    INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature, building_id) VALUES(-10, 'Room1', 1, 22.3, 20.0,2);
+    INSERT INTO ROOM(id, name, floor,building_id) VALUES(-9, 'Room2', 1,3);
 
     INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-10, 'ON', 'Heater1', 2000, -10);
     INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-9, 'ON', 'Heater2', null, -10);
@@ -9,7 +15,4 @@
     INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-8, 'OPEN', 'Window 1', -9);
     INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-7, 'CLOSED', 'Window 2', -9);
 
-    INSERT INTO BUILDING(id,name) VALUES(3,'Administration');
-    INSERT INTO BUILDING(id,name) VALUES(2,'Gymnase');
-    INSERT INTO BUILDING(id,name) VALUES(4,'Secrétariat');
 
